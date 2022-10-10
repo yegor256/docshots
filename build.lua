59b93a517675c24fc8b0acb6ad4932e83596f6a9
Yegor Bugayenko <yegor256@gmail.com>
2022-10-10 09:31:55 +0300

    #1 clean right

--- build.lua
+++ build.lua
@@ -3,7 +3,7 @@ ctanupload = true
 typesetopts = "--interaction=batchmode --shell-escape"
 checkopts = "--interaction=batchmode --shell-escape"
 tagfiles = {"build.lua", "docshots.dtx"}
-cleanfiles = {"*.glo", "*.fls", "*.idx", "*.out", "*.fdb_latexmk", "*.aux", "*.sty", "*.zip", "docshots.pdf"}
+cleanfiles = {"docshots.ds.*", "*.log", "*.glo", "*.fls", "*.idx", "*.out", "*.fdb_latexmk", "*.aux", "*.sty", "*.zip", "docshots.pdf"}
 typesetruns = 2
 
 uploadconfig = {
